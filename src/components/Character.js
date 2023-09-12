c32796483b57dea622613264b7bdbe8a0b0f4514
Eleanor Kelman <ekelman3@gmail.com>
2023-09-12 14:46:44 -0400

    finished second lecture

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+
+function Character( { character } ) {
+
+    const [inGoodMood, setInGoodMood] = useState(character.isHappy)
+
+    console.log(inGoodMood)
+
+    // toggling click event
+    function handleClick() {
+        if (inGoodMood == true) {
+            setInGoodMood(false)
+        }
+        else {
+            setInGoodMood(true)
+        }
+
+        // setInGoodMood(!inGoodMood)
+    }
+
+    // conditional render using toggling state
+    return (
+        <>
+            <img onClick = { handleClick } src = {character.image} alt = {character.name} />
+            {/* <img onClick = { () => setInGoodMood(!inGoodMood) } src = {character.image} alt = {character.name} /> */}
+            <p>Is Eeyore Happy? { inGoodMood ? "Yes!" : "No :(" }</p>
+            {/* { character.isHappy ? <p>Is Eeyore Happy? Yes!</p> : <p>Is Eeyore Happy? No </p> } */}
+        </>
+    )
+
+}
+
+export default Character;
